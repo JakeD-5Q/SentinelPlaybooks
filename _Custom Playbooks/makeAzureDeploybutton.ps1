8f1e809979aa67acfd7705530cd634edac70f145
Jake Derkowski <77984636+JakeD-5Q@users.noreply.github.com>
2022-04-26 22:50:10 -0500

    deploy button

---
+++ _Custom Playbooks/makeAzureDeploybutton.ps1
@@ -0,0 +1,3 @@
+$rawURL = 'https://raw.githubusercontent.com/JakeD-5Q/SentinelPlaybooks/main/_Custom%20Playbooks/Launch-FullMDEInvestigation/template.json?token=GHSAT0AAAAAABTTDQUUPM5BD54XK3PTH2JWYTIXPGA'
+$url = $rawURL
+[uri]::EscapeDataString($url)
