ebb3eb69333354842c8427a210a2b74ffb7ae1d1
Jake Derkowski <77984636+JakeD-5Q@users.noreply.github.com>
2022-06-28 18:54:05 -0500

    clean

--- __TemplateDir/deploy.ps1
+++
@@ -1,2 +0,0 @@
-Import-Module Az.Resources
-New-AzResourceGroupDeployment -ResourceGroupName YourRG -TemplateFile PlaybookName.json -TemplateParameterFile PlaybookName.parameters.json -Name PlaybookName -Verbose
