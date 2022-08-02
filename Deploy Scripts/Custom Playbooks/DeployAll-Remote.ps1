1a5d7d94f9a335b6e1735ccb7ec4022fd7f34f12
Jake Derkowski <77984636+JakeD-5Q@users.noreply.github.com>
2022-08-02 09:53:52 -0500

    fix locations

--- Deploy Scripts/Custom Playbooks/DeployAll-Remote.ps1
+++ Deploy Scripts/Custom Playbooks/DeployAll-Remote.ps1
@@ -1,14 +1,33 @@
+param(
+    [Parameter(Mandatory = $true)]$NamePrefix,
+    [Parameter(Mandatory = $true)]$PlaybookName,
+    [Parameter(Mandatory = $true)]$ResourceGroup,
+    [Parameter(Mandatory = $true)]$Location
+)
+
+$DeployedName = $NamePrefix + ".$PlaybookName"
+
+
+# get the object id of the playbook
+$ID = (Get-AzResource -Name $DeployedName -ResourceType Microsoft.Logic/workflows).Identity.PrincipalId
+$MIGuid = $ID
+$MI = Get-AzureADServicePrincipal -ObjectId $MIGuid
+
+
 # Deploy all of these playbooks without downloading or cloning this repository
 $today = Get-Date -Format "MM-dd-yyyy"
 $suffix = Get-Random -Maximum 100
 $deploySuffix = $today + "_$suffix"
 
+
 # remote deploys
-$deploymentName = "basename_" + $deploySuffix
+$deploymentName = $PlaybookName + $deploySuffix
+
+
 $remoteUrl = ""
-$remoteTemplate = ''
+$remoteTemplate = ""
 New-AzResourceGroupDeployment -Name $deploymentName `
-    -ResourceGroupName $myRg `
+    -ResourceGroupName $ResourceGroup `
     -TemplateUri $remoteUrl `
     -TemplateParameterFile $remoteTemplate
--Verbose
+    -Verbose
